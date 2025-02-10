be5afa2ea9aa4ac364f195f53e4eda12f3e5a899
AksaMJML <akaks0186@gmail.com>
2025-02-10 18:45:34 +0530

    added sample productcart details ✅

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ import ProductCard from './components/productCard'
 function App() {
 
   return (
+    <div>
     <>
-      <ProductCard name="Audio Setup" price = "3500" description="
-      hgfdhhhhaydggggggggggdsssssss" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOt24ovFYohm69e4a1D5ZvV2V93_nBRQvYbQ&s"/>
+      <ProductCard 
+      name="Audio Setup" 
+      price = "3500" 
+      description="hgfdhhhhaydggggggggggdsssssss" 
+      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOt24ovFYohm69e4a1D5ZvV2V93_nBRQvYbQ&s"/>
     </>
+
+    <ProductCard 
+    name="portable sound card kit"
+    price="14000"
+    description="Ships from overseas Products shipped by International sellers are delivered at no additional cost"
+    image="https://img.drz.lazcdn.com/static/lk/p/01145fb0649587205c280deb767ac701.png_400x400q75.png_.webp">
+    </ProductCard>
+    </div>
   )
 }
 
--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -2,10 +2,10 @@ import "./productCard.css";
 export default function ProductCard(props){
     return(
         <div>
-            <img src="https://www.buyzone.lk/wp-content/uploads/2023/01/194-800x800.jpg"/>
-            <span>Jbl audio mini</span>
-            <span>LKR.3000</span>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque amet quaerat autem?.</p>
+            <img src={props.image}/>
+            <span>{props.name}</span>
+            <span>LKR.{props.price}</span>
+            <p>{props.description}</p>
         </div>
     )
 }
