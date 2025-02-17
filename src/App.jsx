17a7f203f0cf12481965fb7ac216955c5bc1dad7
AksaMJML <akaks0186@gmail.com>
2025-02-17 13:43:10 +0530

    creating admin panel 🧑‍💻

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,9 @@ import ProductCard from './components/productCard'
 function App() {
 
   return (
-    <div>
-    <>
-      <ProductCard 
-      name="Audio Setup" 
-      price = "3500" 
-      description="hgfdhhhhaydggggggggggdsssssss" 
-      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOt24ovFYohm69e4a1D5ZvV2V93_nBRQvYbQ&s"/>
-    </>
-
-    <ProductCard 
-    name="portable sound card kit"
-    price="14000"
-    description="Ships from overseas Products shipped by International sellers are delivered at no additional cost"
-    image="https://img.drz.lazcdn.com/static/lk/p/01145fb0649587205c280deb767ac701.png_400x400q75.png_.webp">
-    </ProductCard>
+    <div className='w-full h-screen flex'>
+      <div className='w-[300px] h-full bg-green-200'></div> 
+      <div className='w-full bg-red-400'></div>
     </div>
   )
 }
