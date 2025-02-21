3715d3409a982bee6679f979c37bdc5c043ac34e
AksaMJML <akaks0186@gmail.com>
2025-02-21 20:54:03 +0530

    email password atributes modified 💥

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,20 @@
+import "./login.css";
 import { useState } from "react"
 
 export default function LoginPage(){
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
     return(
-        <div className=" w-full h-screen flex justify-center items-center">
-        <div className="w-[400px] h-[400px]"></div>
+        <div className="bg-picture w-full h-screen flex justify-center items-center">
+        <div className="w-[400px] h-[400px] backdrop-blur-xl rounded-2xl flex flex-col justify-center items-center relative">
+            <img src="logo2.png" alt="logo" className="w-[100px] h-[100px] object-cover top-1"/>
+            <input type="email" placeholder="Email" className="w-[300px] h-[50px] bg-transparent border-b-2 border-white text-white text-[15px] outline-none"></input>
+            <input type="password" placeholder="Password" className="w-[300px] h-[50px] bg-transparent border-b-2 border-white text-white text-[15px] outline-none"></input>
+            <button  className="w-[200px] h-[50px] bg-[#efac38] text-[25px] text-white rounded-lg my-5">
+                Login
+            </button>
+
+        </div>
         </div>
     )
 }
