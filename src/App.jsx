70d9d845f9ae33bbba8d5a9e75f0cdf0ef3e2081
AksaMJML <akaks0186@gmail.com>
2025-02-21 19:33:30 +0530

    tested useState hooks and onclick event for bttons for sample test 💥✅

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import ProductCard from './components/productCard' ;
 import AdminPage from './pages/admin/adminPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home/homePage';
+import Testing from './components/testing';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes path="/">
+        <Route path='/testing' element={<Testing/>}></Route>
         <Route path='/admin/*' element={<AdminPage/>}/>
         <Route path='/*' element={<HomePage/>}></Route>
       </Routes>
